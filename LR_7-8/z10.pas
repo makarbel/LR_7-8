1ed80c50473485522ccc22300c313088cb96fa30
makarbel <144238241+makarbel@users.noreply.github.com>
2023-11-16 10:56:47 +0300

    Add files via upload

---
+++ LR_7-8/z10.pas
@@ -0,0 +1,19 @@
+﻿Program Zad10;
+var st,last:string; dlina,i,x,k,first,m:integer;
+begin
+writeln ('Введите строку');
+readln (st);
+dlina := length(st);
+for i := 1 to dlina do begin
+  if (st[i] = 'a') and (st[i+1] = 'b') and (st[i+2] = 'c') then begin
+    st[i] := 'w';
+    st[i+1] := 'w';
+    st[i+2] := 'w';
+  end
+  else
+    if i = dlina then begin
+      st := st + 'zzz';
+    end;
+end;
+writeln (st);
+end.
---
+++ LR_7-8/z16.pas
@@ -0,0 +1,9 @@
+﻿program zad16;
+var st:string;
+begin
+  writeln('Введи строку ');
+  readln(st);
+  delete(st,1,57);
+  delete(st,41,100);
+  writeln(st);
+end.  
